6c9087df545fe72d7dd734fe2938989f5845c1f5
cocoa <132428612+cocoalatte7119@users.noreply.github.com>
2023-05-15 04:09:55 +0900

    Add files via upload

--- src/js/result.js
+++ src/js/result.js
@@ -1,25 +1,31 @@
 $(document).ready(function () {
+    let selectScores = 0;
+    let score = 0;
+    let prevScore = 0;
     let total = 0;
 
     // クイズの各回答に対して、クリックイベントを設定する
     $('.select').click(function () {
         // 選択された回答の点数を加算する
-        total += parseInt($(this).data('points'));
+        selectScores = parseInt($(this).data('point'));
+        prevScore = score;
+        score += selectScores;
+
+        console.log("選択された点数：" + selectScores);
+        console.log("合計点：" + score);
+        console.log("1つ前の点数：" + prevScore);
     });
 
-    // 提出ボタンにクリックイベントを設定する
-    $('#submit').click(function () {
-        // 結果を取得し、表示する
-        $.getJSON('result.json', function (data) {
-            let resultText = '';
+    $('.back').click(function () {
+        // 選択された回答の点数を加算する
+        score = prevScore;
+        console.log("合計点：" + score);
+    });
 
-            $.each(data.results, function (i, result) {
-                if (total >= result.minPoints) {
-                    resultText = result.text;
-                }
-            });
-
-            $('#result-text').text(resultText);
-        });
+    $('.submit').click(function () {
+        // 選択された回答の点数を加算する
+        total = score;
+        $('.result_txt p').text(total);
+        console.log(total)
     });
 });
