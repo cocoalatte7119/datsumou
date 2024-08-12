a9024f8dbeedbf027a9e280f814a310b75f5e493
Shunya Tanaka <soccerplayer161236@gmail.com>
2024-08-12 15:08:52 +0900

    saisin

--- src/js/answer.js
+++ src/js/answer.js
@@ -2,18 +2,14 @@ $(function () {
 
     $('.select').on('click', function () {
         let parent = $(this).parents(".question_contents");
-        console.log(parent);
         let nextParent = parent.next();
-        console.log(nextParent);
-        parent.removeClass("is-active");
-        nextParent.addClass("is-active");
+        parent.removeClass("is-active").fadeOut();
+        nextParent.addClass("is-active").fadeIn();
     })
 
     $('.back').on('click', function () {
         let parent = $(this).parents(".question_contents");
-        console.log(parent);
         let prevParent = parent.prev();
-        console.log(prevParent);
         parent.removeClass("is-active");
         prevParent.addClass("is-active");
     })
--- src/js/result.js
+++ src/js/result.js
@@ -4,6 +4,12 @@ $(document).ready(function () {
     let prevScore = 0;
     let total = 0;
 
+    function loadJSON(callback) {
+        $.getJSON('src/json/dataBase.json', function (data) {
+            callback(data);
+        });
+    }
+
     // クイズの各回答に対して、クリックイベントを設定する
     $('.select').click(function () {
         // 選択された回答の点数を加算する
@@ -23,9 +29,32 @@ $(document).ready(function () {
     });
 
     $('.submit').click(function () {
-        // 選択された回答の点数を加算する
         total = score;
-        $('.result_txt p').text(total);
-        console.log(total)
+        // $('.result_txt p').text(total);
+        // console.log(total);
+        // 選択された回答の点数を加算する
+        function searchDataAndDisplay(number) {
+            loadJSON(function (data) {
+                // 番号が一致するデータを検索
+                let searchNumber = parseInt(number, 10);
+                let matchingData = data.find(function (item) {
+                    return item.id === searchNumber;
+                });
+
+                if (matchingData && matchingData.店名 !== "") {
+                    // 該当するデータをHTMLに表示
+                    $('.result_txt_ttl').html(matchingData.店名);
+                    $('.result_txt_point-1').html(matchingData.概要1);
+                    $('.result_txt_point-2').html(matchingData.概要2);
+                    $('.result_txt_point-3').html(matchingData.概要3);
+                    $('.result_img').html(matchingData.アフィリエイトURL);
+                }
+                else {
+                    // 該当するデータが見つからなかった場合の処理
+                    window.location.href = 'error.html';
+                }
+            });
+        }
+        searchDataAndDisplay(total);
     });
 });
